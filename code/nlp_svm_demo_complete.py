eaf21a8647e98f266f7d5c6fc75ee5c8e49c1875
Dante Razo <dante@razonet.org>
2020-02-27 18:30:52 -0500

    Final. Clean up + remove unnecessary automation functionality + hard-code paramas

--- code/nlp_svm_demo_complete.py
+++ code/nlp_svm_demo_complete.py
@@ -28,66 +28,41 @@ def get_data():
     for i in range(0, len(y_test)):
         y_test[i] = int(y_test[i])
 
+    # Not worrying about X_dev, y_dev split for now...
+
     a.close()
     return X_train, X_test, y_train, y_test
 
 
-# Feature engineering: vectorizer
-# ML models need features, not just whole tweets
-print("COUNTVECTORIZER CONFIG\n----------------------")
-analyzer = input("Please enter analyzer: ")
-ngram_upper_bound = input("Please enter ngram upper bound(s): ").split()
+""" FEATURE ENGINEERING """
+# params
+analyzer = "word"  # CV. "word" OR "char"
+ngram_upper_bound = 3  # CV. Probably best to keep it lower
+kernel = "linear"  # SVM. "linear" OR "poly" OR "rbf" OR "sigmoid"
+gamma = "auto"  # SVM. kernel coefficient for rbf, poly, sigmoid kernels
 
-for i in ngram_upper_bound:
-    X_train, X_test, y_train, y_test = get_data()
-    verbose = False  # print statement flag
+# Get data, assign it to variables
+X_train, X_test, y_train, y_test = get_data()
 
-    vec = CountVectorizer(analyzer=analyzer, ngram_range=(1, int(i)))
-    print("\nFitting CV...") if verbose else None
-    X_train = vec.fit_transform(X_train)
-    X_test = vec.transform(X_test)
+# Convert plaintext into features
+# ML models need features, not just plaintext. The CountVectorizer converts the latter into the former
+vec = CountVectorizer(analyzer=analyzer, ngram_range=(1, int(ngram_upper_bound)))
+print("Fitting CV...")
+X_train = vec.fit_transform(X_train)
+X_test = vec.transform(X_test)
 
-    # Shuffle data (keeps indices)
-    X_train, y_train = shuffle(X_train, y_train)
-    X_test, y_test = shuffle(X_test, y_test)
+# Shuffle data (keeps indices)
+X_train, y_train = shuffle(X_train, y_train)
+X_test, y_test = shuffle(X_test, y_test)
 
-    # Fitting the model
-    print("Training SVM...") if verbose else None
-    svm = SVC(kernel="linear", gamma="auto")  # TODO: tweak params
-    svm.fit(X_train, y_train)
-    print("Training complete.") if verbose else None
+# Fitting the model
+print("Training SVM...")
+svm = SVC(kernel=kernel, gamma="auto")  # TODO: tweak params
+svm.fit(X_train, y_train)
+print("Training complete.")
 
-    # Testing + results
-    """
-    rand_acc = sklearn.metrics.balanced_accuracy_score(y_test, [random.randint(0, 1) for x in range(0, len(y_test))])
-    acc_score = sklearn.metrics.accuracy_score(y_test, svm.predict(X_test))
-
-    print(f"\nResults for ({analyzer}, ngram_range(1,{i}):")
-    print(f"Baseline Accuracy: {rand_acc:}")  # random
-    print(f"Testing Accuracy:  {acc_score:}")
-    """
-
-    # Testing + results
-    print(f"Classification Report [{analyzer}, ngram_range(1,{i})]:\n "
-          f"{sklearn.metrics.classification_report(y_test, svm.predict(X_test), digits=6)}")
-
-""" RESULTS & DOCUMENTATION
-# KERNEL RESULTS (gamma="auto", analyzer=word, ngram_range(1,3))
-linear:  0.8549618320610687
-rbf:     0.6844783715012722
-poly:    0.6844783715012722
-sigmoid: 0.6844783715012722
-precomputed: N/A, not supported
-
-# CV PARAM TESTING (kernel="linear")
-word, ngram_range(1,2):  0.8606870229007634
-word, ngram_range(1,3):  0.8549618320610687
-word, ngram_range(1,5):  0.8473282442748091
-word, ngram_range(1,10): 0.8358778625954199
-word, ngram_range(1,20): 0.8326972010178118
-char, ngram_range(1,2):  0.8225190839694656
-char, ngram_range(1,3):  0.8206106870229007
-char, ngram_range(1,5):  0.8473282442748091
-char, ngram_range(1,10): 0.8473282442748091
-char, ngram_range(1,20): 0.8575063613231552
-"""
+# Testing + results
+rand_acc = sklearn.metrics.balanced_accuracy_score(y_test, [random.randint(1, 2) for x in range(0, len(y_test))])
+print(f"Baseline Accuracy: {rand_acc:}\n")  # random baseline
+print(f"Classification Report [{kernel} kernel, {analyzer} analyzer, ngram_range(1,{ngram_upper_bound})]:\n "
+      f"{sklearn.metrics.classification_report(y_test, svm.predict(X_test), digits=6)}")  # report
